b8b1f916c7625a68d6e356be9ae43672a9fe9edf
Nick Gordon <ngordon22293@gmail.com>
2019-03-22 22:00:23 -0600

    Iterate on googles extension tutorial

---
+++ app/background.js
@@ -0,0 +1,18 @@
+/*
+* Nick Gordon - 2018
+*/
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.storage.sync.set({ color: '#3aa757' }, () => {
+        console.log("The color is green.");
+    });
+
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
+        chrome.declarativeContent.onPageChanged.addRules([{
+            conditions: [new chrome.declarativeContent.PageStateMatcher({
+                pageUrl: { hostEquals: 'www.google.com' },
+            })
+            ],
+            actions: [new chrome.declarativeContent.ShowPageAction()]
+        }]);
+    });
+});
